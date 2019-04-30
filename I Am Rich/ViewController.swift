d1285b80cf6eb4b40139cc86a1f768ebd75a702c
Yvonne ariga <yvonnekemi@hotmail.com>
2019-04-30 08:48:35 +0300

    added image to app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by MobiDev on 30/04/2019.
+//  Copyright © 2019 MobiDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
